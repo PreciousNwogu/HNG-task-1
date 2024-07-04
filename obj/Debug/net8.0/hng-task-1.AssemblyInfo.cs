5f4f27a65f70c9019b611f7d203fe349a97cc586
PreciousNwogu <140530653+PreciousNwogu@users.noreply.github.com>
2024-07-04 16:02:35 -0400

    completed

--- obj/Debug/net8.0/hng-task-1.AssemblyInfo.cs
+++ obj/Debug/net8.0/hng-task-1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("hng-task-1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1267fd62c2cdd6d2312a5cb08ea49901f492e7c")]
 [assembly: System.Reflection.AssemblyProductAttribute("hng-task-1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("hng-task-1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
